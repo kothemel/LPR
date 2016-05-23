8faca651b0937962860ce3623a822eb7e30ef9a3
Konstantinos Themelis <kothemel@gmail.com>
2016-05-23 12:19:00 +0300

    LPR c++ code

---
+++ kostas/CMakeFiles/locate.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/locate.out.dir/locateLP.cpp.o"
+  "locate.out.pdb"
+  "locate.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/locate.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ kostas/CMakeFiles/locateNO.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/locateNO.out.dir/locateLP_noThresh.cpp.o"
+  "locateNO.out.pdb"
+  "locateNO.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/locateNO.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
